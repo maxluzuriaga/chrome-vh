dfa3569be707eeba8f378b566c8044fc7c117c6d
Max Luzuriaga <max@luzuriaga.com>
2014-12-29 16:07:05 -0500

    Changed from page action to browser action.

--- js/background.js
+++ js/background.js
@@ -1,11 +1,3 @@
-var myid = chrome.runtime.id;
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if (tab.url == "chrome://history/") {
-		chrome.pageAction.show(tabId);
-	}
-});
-
-chrome.pageAction.onClicked.addListener(function(activeTab) {
+chrome.browserAction.onClicked.addListener(function(activeTab) {
 	chrome.tabs.create({url: "index.html"});
 });
