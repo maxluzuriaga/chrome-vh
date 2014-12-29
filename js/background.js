f90f108fba506b2b52bf5bbc3428acac643ca857
Max Luzuriaga <max@luzuriaga.com>
2014-12-29 13:37:24 -0500

    Favicons.

--- js/background.js
+++ js/background.js
@@ -1,12 +1,11 @@
 var myid = chrome.runtime.id;
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if (tab.url == "chrome://history/" || tab.url.indexOf(myid) > -1) {
+	if (tab.url == "chrome://history/") {
 		chrome.pageAction.show(tabId);
 	}
 });
 
 chrome.pageAction.onClicked.addListener(function(activeTab) {
-	alert(activeTab);
 	chrome.tabs.create({url: "index.html"});
 });
